8639d23d965aefcf970b700886ae4f508b4e630a
Yonas-meharites <yonas.meharites@gmail.com>
2024-09-18 19:22:07 +0200

    latest-pygments

--- source/conf.py
+++ source/conf.py
@@ -40,7 +40,7 @@ images_config = {
 
 # The name of the Pygments (syntax highlighting) style to use.
 # pygments_style = 'sphinx'
-pygments_style = 'abap'
+pygments_style = 'xcode'
 
 # html_css_files = ['css/wazuh-custom.css']  # Link to custom CSS
 
